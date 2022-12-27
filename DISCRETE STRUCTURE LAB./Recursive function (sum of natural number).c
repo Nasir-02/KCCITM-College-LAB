ce14f87a116c74714f0df2bdeba09f07b1ca01f0
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2022-12-27 23:00:04 +0530

    Create Recursive function (sum of natural number).c

---
+++ DISCRETE STRUCTURE LAB./Recursive function (sum of natural number).c
@@ -0,0 +1,17 @@
+// Exp 9.Recursive function (sum of natural number)
+
+#include <stdio.h>
+int sum(int n);
+int main(){
+int number, result;
+ printf("Enter a positive integer: "); 
+ scanf("%d", &number);
+ result = sum(number);
+ printf("sum = %d", result); 
+ return 0;
+}
+int sum(int n) {
+ if (n != 0) // sum() function calls itself 
+return n + sum(n-1); 
+else return n;
+
