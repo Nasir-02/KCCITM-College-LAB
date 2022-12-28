7e9d571e22f1ad26170876092d245919245a5b62
NASIR HUSAIN <104553289+Nasir-02@users.noreply.github.com>
2022-12-28 22:02:36 +0530

    Create  C prog to find Cartesian product of two sets.c

---
+++ DISCRETE STRUCTURE LAB./C prog to find Cartesian product of two sets.c
@@ -0,0 +1,28 @@
+// Exp6. C prog to find Cartesian product of two sets
+
+#include<stdio.h>
+#include<math.h>
+int main(){
+int a[50],b[50],c[50],i,s1,s2,j,k; 
+printf("Enter how many elements in set 1\n"); 
+scanf("%d",&s1);
+printf("Enter how many elements in set 2\n"); 
+scanf("%d",&s2); 
+printf("Enter elements of set 1\n"); 
+  for(i=0;i<s1;i++) { 
+     scanf("%d",&a[i]);
+}
+printf("Enter elements of set 2\n"); 
+   for(i=0;i<s2;i++) { 
+    scanf("%d",&b[i]); } 
+printf("cartessian product="); 
+printf("{"); 
+   for(i=0;i<s1;i++) { 
+      for(j=0;j<s2;j++) {
+          printf("(%d,%d)",a[i],b[j]); 
+          printf(",");
+          }
+   } 
+printf("}");
+return 0;
+}
